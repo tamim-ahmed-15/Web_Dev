be7bee1a5640a440ccbf771d61e79b948bfe968c
Tamim Ahmed <166158805+tamim-ahmed-15@users.noreply.github.com>
2025-03-08 19:56:40 +0600

    Basic Math Calculator

---
+++ Basic_Math_Calculator/calculator.js
@@ -0,0 +1,21 @@
+let input=document.getElementById('inputbox');
+let buttons=document.querySelectorAll('button');
+let string="";
+let arr=Array.from(buttons);
+arr.forEach(button =>{
+    button.addEventListener('click', (e)=>{
+        if(e.target.innerHTML== '='){
+            string = eval(string);
+        }
+        else if(e.target.innerHTML== 'AC'){
+            string="";
+        }
+        else if(e.target.innerHTML=='DEL'){
+            string=string.substring(0,string.length-1);
+        }
+        else {
+            string += e.target.innerHTML;
+        }
+        input.value = string;
+    })
+})
